f22639861d09ab295a24efde350fd45f1a53df00
fdhgfdg <fgdfg>
2017-04-04 07:33:51 +0700

    democdw1

---
+++ chuademo/product.php
@@ -0,0 +1,93 @@
+<html>
+<head>
+    <meta charset="UTF-8">
+    <title></title>
+    <link href="css/font-awesome.min.css" rel="stylesheet" type="text/css" />
+    <link href="css/bootstrap.min.css" rel="stylesheet" type="text/css" />
+   
+    <link href="css/styles_danhsach.css" rel="stylesheet" type="text/css" />
+    <link href="css/styles_chitiet.css" rel="stylesheet" type="text/css" />
+    
+    <script src="js/jquery-2.1.4.min.js" type="text/javascript" ></script> 
+    <script src="js/bootstrap.js" type="text/javascript" ></script>
+    
+    
+    
+    
+</head>
+<body>
+    <div class="type_32_d">
+        <div class="tp-breadcrumb st_1479475285">
+            <div class="container">
+                <div class="col-md-offset-3 col-md-5">
+                    <div class="breadcrumb">
+                        <span typeof="v:Breadcrumb">
+                            <a rel="v:url" property="v:title" title="Go to STEEL." href="#" class="home">STEEL</a></span> &gt; <span typeof="v:Breadcrumb"><span property="v:title">Service</span>
+
+                        </span>                        
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="type_32_c">
+        <div class="container1">
+            <div class="col-sm-12">
+                <article  class=" page">
+                    <div class=" vc_custom_1437539259227">
+                        <div class="col-sm-12"> 
+                            <div class=" wpb_content_element  vc_custom_1437539195297">
+                                <h1 style="text-align: center;">We are Providing good services</h1>
+                            </div>
+                            <div class="tp-service-page">
+                                <div class="service-list normal">
+                                        <div class="col-md-6 col-xs-12 ">
+                                            <div class="tp-service-block">
+                                                <div class="service-block-pic image-scale">
+                                                   <img src="images/1.jpg" alt=""/>
+                                                </div>
+                                                <h2 class="service-title"><a href="#">adasdas</a></h2>
+                                                <p>afsf</p>
+                                                <a href="./detail" class="btn btn-outline">Read More</a>
+                                            </div>
+                                        </div>
+                                      <div class="col-md-6 col-xs-12 ">
+                                            <div class="tp-service-block">
+                                                <div class="service-block-pic image-scale">
+                                                    <img src="images/2.jpg" alt=""/>
+                                                </div>
+                                                <h2 class="service-title"><a href="#">adasdas</a></h2>
+                                                <p>afsf</p>
+                                                <a href="./detail" class="btn btn-outline">Read More</a>
+                                            </div>
+                                        </div>
+                                      <div class="col-md-6 col-xs-12 ">
+                                            <div class="tp-service-block">
+                                                <div class="service-block-pic image-scale">
+                                                    <img src="images/3.jpg" alt=""/>
+                                                </div>
+                                                <h2 class="service-title"><a href="#">adasdas</a></h2>
+                                                <p>afsf</p>
+                                                <a href="./detail" class="btn btn-outline">Read More</a>
+                                            </div>
+                                        </div>
+                                      <div class="col-md-6 col-xs-12 ">
+                                            <div class="tp-service-block">
+                                                <div class="service-block-pic image-scale">
+                                                    <img src="images/4.jpg" alt=""/>
+                                                </div>
+                                                <h2 class="service-title"><a href="#">adasdas</a></h2>
+                                                <p>afsf</p>
+                                                <a href="./detail" class="btn btn-outline">Read More</a>
+                                            </div>
+                                        </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </article>
+            </div>
+        </div>
+    </div>
+</body>
+</html>
